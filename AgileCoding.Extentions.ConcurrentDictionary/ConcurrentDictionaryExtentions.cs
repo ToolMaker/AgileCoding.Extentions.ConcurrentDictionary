7c2739e677c2ad45c6e974335c6908fe4f3e66b1
Ernie <gunning.ernie@gmail.com>
2021-06-01 19:16:08 +0200

    Project Start

---
+++ AgileCoding.Extentions.ConcurrentDictionary/ConcurrentDictionaryExtentions.cs
@@ -0,0 +1,22 @@
+﻿namespace AgileCoding.Extentions.ConcurrentDictionaries
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+
+    public static class ConcurrentDictionaryExtentions
+    {
+        public static Dictionary<TKeyType, TValueType2> ToDictionary<TKeyType, TValueType1, TValueType2>
+            (this ConcurrentDictionary<TKeyType, TValueType1> self, Func<KeyValuePair<TKeyType, TValueType1>, KeyValuePair<TKeyType, TValueType2>> mapperFunction)
+        {
+            Dictionary<TKeyType, TValueType2> resultDictionary = new Dictionary<TKeyType, TValueType2>();
+            foreach (KeyValuePair<TKeyType, TValueType1> item in self)
+            {
+                var result = mapperFunction(item);
+                resultDictionary.Add(result.Key, result.Value);
+            }
+
+            return resultDictionary;
+        }
+    }
+}
